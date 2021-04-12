48cb41086c8c474a5383fe8364a72549391edac9
andrii-pulya <andrey.predein@gmail.com>
2021-04-12 19:33:00 +0300

    modal closed
    issue with overlay

--- 08-gallery-items.js
+++ 08-gallery-items.js
@@ -4,13 +4,16 @@ const refs = {
   gallery: document.querySelector('.js-gallery'),
   lightbox: document.querySelector('.js-lightbox'),
   lightboxImage: document.querySelector('.lightbox__image'),
-
+  closeButton: document.querySelector('.lightbox__button'),
+  modalOverlay: document.querySelector('.lightbox__overlay'),
 };
 
 const galleryMarkup = createGalleryMarkup(cards);
 refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
 refs.gallery.addEventListener('click', onCardOpen);
+refs.lightbox.addEventListener('click', modalClose);
+
 
 function createGalleryMarkup(cards) {
   return cards
@@ -45,3 +48,24 @@ function onCardOpen(evt) {
   refs.lightbox.classList.add('is-open');
   refs.lightboxImage.src = `${evt.target.dataset.source}`;
 };
+
+function modalClose(evt) {
+  evt.preventDefault();
+  // console.log(evt.target);
+  
+if (!evt.target.classList.contains('lightbox__button') || !evt.target.classList.contains('lightbox__overlay')) {
+    return
+  }
+    refs.lightbox.classList.remove('is-open');
+    refs.lightboxImage.src = '';
+}
+
+// if (!evt.target.classList.contains('lightbox__overlay')) {
+//     return
+//   } else {
+
+// document.addEventListener('keydown', function(event) {
+//   if (event.code == 'KeyZ' && (event.ctrlKey || event.metaKey)) {
+//     alert('Отменить!')
+//   }
+// });
